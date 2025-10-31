68dc6fd82324ad513599aea397758f6b5de9288d
조상준 <juncho12011201@gmail.com>
2025-10-31 14:06:48 +0100

    feat(view): 당첨 번호 입력 로직 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,8 +2,11 @@ package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.List;
+
 public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT_GUIDE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_INPUT_GUIDE = "당첨 번호를 입력해 주세요.";
 
     private final InputParser inputParser;
 
@@ -15,5 +18,11 @@ public class InputView {
         System.out.println(PURCHASE_AMOUNT_INPUT_GUIDE);
         String input = readLine();
         return inputParser.parsePurchaseAmount(input);
+    }
+
+    public List<Integer> readWinningNumber() {
+        System.out.println(WINNING_NUMBER_INPUT_GUIDE);
+        String input = readLine();
+        return inputParser.parseWinningNumber(input);
     }
 }
