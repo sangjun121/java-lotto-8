114efb7a66b251a0fc4481b3656e543197624738
조상준 <juncho12011201@gmail.com>
2025-10-31 17:41:37 +0100

    refactor(test): 불필요한 상수 선언 제거

--- src/test/java/lotto/domain/WinningCheckerTest.java
+++ src/test/java/lotto/domain/WinningCheckerTest.java
@@ -6,12 +6,11 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class WinningCheckerTest {
-    private static final int VALID_BONUS_NUMBER = 45;
 
     @Test
     void 로또_번호가_당첨_번호와_3개_일치하는_경우_일치_개수는_3이_반환된다() {
         WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6));
-        BonusNumber bonusNumber = BonusNumber.of(VALID_BONUS_NUMBER, winningNumber);
+        BonusNumber bonusNumber = BonusNumber.of(45, winningNumber);
         WinningChecker winningChecker = new WinningChecker(winningNumber, bonusNumber);
 
         Lotto lotto = Lotto.from(List.of(1, 2, 3, 10, 11, 12));
