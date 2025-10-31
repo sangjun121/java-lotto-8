5ef8110815a73b5c92a5e16cffca77286df56464
조상준 <juncho12011201@gmail.com>
2025-10-31 17:55:30 +0100

    test(domain): 로또 번호가 보너스 번호와 일치하는 경우 참값 반환 테스트 작성

--- src/test/java/lotto/domain/WinningCheckerTest.java
+++ src/test/java/lotto/domain/WinningCheckerTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-public class WinningCheckerTest {
+class WinningCheckerTest {
 
     @Test
     void 로또_번호가_당첨_번호와_3개_일치하는_경우_일치_개수는_3이_반환된다() {
@@ -18,5 +18,18 @@ public class WinningCheckerTest {
         int result = winningChecker.calculateRank(lotto).getMatchCount();
 
         assertThat(result).isEqualTo(3);
+    }
+
+    // TODO: 우회 테스트. 수정 필요
+    @Test
+    void 로또_번호가_보너스_번호와_일치하는_경우_참_값이_반환된다() {
+        WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = BonusNumber.of(45, winningNumber);
+
+        Lotto lotto = Lotto.from(List.of(7, 8, 9, 10, 11, 45));
+
+        boolean result = lotto.getNumbers().contains(bonusNumber.getValue());
+
+        assertThat(result).isTrue();
     }
 }
