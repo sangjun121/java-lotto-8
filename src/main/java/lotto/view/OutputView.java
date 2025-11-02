23541061b386648538d5da4ab009a5a7c9c86ee6
조상준 <juncho12011201@gmail.com>
2025-11-02 13:21:10 +0100

    feat(view): 로또 전체 목록 출력 메소드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,15 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String LOTTO_COUNT_MESSAGE = "\n%d개를 구매했습니다.";
 
     public void printLottoCount(int lottoCount) {
         System.out.printf(LOTTO_COUNT_MESSAGE, lottoCount);
+    }
+
+    public void printLottos(String lottos) {
+        System.out.printf(lottos);
     }
 }
