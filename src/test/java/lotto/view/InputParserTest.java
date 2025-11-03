5a5babe32eced8ba273b46d8798ef06117317aa6
조상준 <juncho12011201@gmail.com>
2025-11-03 01:32:09 +0100

    refactor(test): 입력 문자열 null, empty에 대한 테스트를 위해 @NullAndEmptySource 어노테이션 선언

--- src/test/java/lotto/view/InputParserTest.java
+++ src/test/java/lotto/view/InputParserTest.java
@@ -9,6 +9,7 @@ import lotto.exception.InvalidInputException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class InputParserTest {
@@ -46,18 +47,10 @@ class InputParserTest {
         assertThat(lottoNumbers).containsExactly(1, 2, 3, 4, 5, 6);
     }
 
-    @Test
-    void 로또_구입_금액_입력_문자열이_null인_경우_예외가_발생한다() {
-        String input = null;
-
-        assertThatThrownBy(() -> inputParser.parsePurchaseAmount(input))
-                .isInstanceOf(InvalidInputException.class)
-                .hasMessage(LottoError.PURCHASE_AMOUNT_INPUT_NULL_OR_BLANK.getMessage());
-    }
-
     @ParameterizedTest
-    @ValueSource(strings = {"", "  "})
-    void 로또_구입_금액_입력_문자열이_empty거나_blank인_경우_예외가_발생한다(String input) {
+    @NullAndEmptySource
+    @ValueSource(strings = {"  "})
+    void 로또_구입_금액_입력_문자열이_null이거나_공백인_경우_예외가_발생한다(String input) {
         assertThatThrownBy(() -> inputParser.parsePurchaseAmount(input))
                 .isInstanceOf(InvalidInputException.class)
                 .hasMessage(LottoError.PURCHASE_AMOUNT_INPUT_NULL_OR_BLANK.getMessage());
@@ -71,18 +64,10 @@ class InputParserTest {
                 .hasMessage(LottoError.PURCHASE_AMOUNT_INPUT_NOT_NUMBER.getMessage());
     }
 
-    @Test
-    void 당첨_번호_입력_문자열이_null인_경우_예외가_발생한다() {
-        String input = null;
-
-        assertThatThrownBy(() -> inputParser.parseWinningNumber(input))
-                .isInstanceOf(InvalidInputException.class)
-                .hasMessage(LottoError.WINNING_NUMBER_INPUT_NULL_OR_BLANK.getMessage());
-    }
-
     @ParameterizedTest
-    @ValueSource(strings = {"", "  "})
-    void 당첨_번호_입력_문자열이_empty거나_blank인_경우_예외가_발생한다(String input) {
+    @NullAndEmptySource
+    @ValueSource(strings = {"  "})
+    void 당첨_번호_입력_문자열이_null이거나_공백인_경우_예외가_발생한다(String input) {
         assertThatThrownBy(() -> inputParser.parseWinningNumber(input))
                 .isInstanceOf(InvalidInputException.class)
                 .hasMessage(LottoError.WINNING_NUMBER_INPUT_NULL_OR_BLANK.getMessage());
@@ -112,18 +97,10 @@ class InputParserTest {
                 .hasMessage(LottoError.WINNING_NUMBER_INPUT_NOT_NUMERIC.getMessage());
     }
 
-    @Test
-    void 보너스_번호_입력_문자열이_null인_경우_예외가_발생한다() {
-        String input = null;
-
-        assertThatThrownBy(() -> inputParser.parseBonusNumber(input))
-                .isInstanceOf(InvalidInputException.class)
-                .hasMessage(LottoError.BONUS_NUMBER_INPUT_NULL_OR_BLANK.getMessage());
-    }
-
     @ParameterizedTest
-    @ValueSource(strings = {"", "  "})
-    void 보너스_번호_입력_문자열이_empty거나_blank인_경우_예외가_발생한다(String input) {
+    @NullAndEmptySource
+    @ValueSource(strings = {"  "})
+    void 보너스_번호_입력_문자열이_null이거나_공백인_경우_예외가_발생한다(String input) {
         assertThatThrownBy(() -> inputParser.parseBonusNumber(input))
                 .isInstanceOf(InvalidInputException.class)
                 .hasMessage(LottoError.BONUS_NUMBER_INPUT_NULL_OR_BLANK.getMessage());
