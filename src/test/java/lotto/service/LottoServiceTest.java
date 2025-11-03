0f8646438d43ed0d762446591323d73805bf4fc6
조상준 <juncho12011201@gmail.com>
2025-11-03 12:47:10 +0100

    test(service): 로또 당첨 개수 반환 정상 테스트 반영

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,19 +1,40 @@
 package lotto.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
+import java.util.List;
 import lotto.config.AppConfig;
+import lotto.controller.dto.WinningStatistic;
 import lotto.domain.Lottos;
+import lotto.domain.vo.BonusNumber;
+import lotto.domain.vo.Lotto;
 import lotto.domain.vo.PurchaseAmount;
+import lotto.domain.vo.WinningNumber;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LottoServiceTest {
+    private static final List<Integer> VALID_WINNING_NUMBER = List.of(1, 2, 3, 4, 5, 6);
+    private static final int VALID_BONUS_NUMBER = 7;
+
     private LottoService lottoService;
+    private WinningNumber validWinningNumber;
+    private BonusNumber validBonusNumber;
 
     @BeforeEach
     void setUp() {
         this.lottoService = AppConfig.getInstance().lottoService();
+        this.validWinningNumber = WinningNumber.from(VALID_WINNING_NUMBER);
+        this.validBonusNumber = BonusNumber.from(VALID_BONUS_NUMBER, validWinningNumber);
+    }
+
+    private Lottos createExampleLottos() {
+        Lotto first = Lotto.from(List.of(1, 2, 3, 4, 5, 6));
+        Lotto second = Lotto.from(List.of(1, 2, 3, 4, 5, 7));
+        Lotto third = Lotto.from(List.of(1, 2, 3, 4, 5, 8));
+        Lotto none = Lotto.from(List.of(10, 11, 12, 13, 14, 15));
+        return new Lottos(List.of(first, second, third, none));
     }
 
     @Test
@@ -24,5 +45,22 @@ class LottoServiceTest {
         int resultLottosSize = lottos.getValues().size();
 
         assertThat(resultLottosSize).isEqualTo(10);
+    }
+
+    @Test
+    void 로또의_당첨_순위_개수를_올바르게_반환한다() {
+        Lottos lottos = createExampleLottos();
+        List<WinningStatistic> result = lottoService.calculateWinningStatistic(lottos, validWinningNumber,
+                validBonusNumber);
+
+        assertThat(result)
+                .extracting("matchCount", "count", "isBonus")
+                .containsExactlyInAnyOrder(
+                        tuple(3, 0, false),
+                        tuple(4, 0, false),
+                        tuple(5, 1, false),
+                        tuple(5, 1, true),
+                        tuple(6, 1, false)
+                );
     }
 }
